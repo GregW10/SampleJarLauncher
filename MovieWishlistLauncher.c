b951ae07140a5e36f17d114a4b6607e61b973fef
El Macho Menos de C++ <mario.garwh@gmail.com>
2022-09-04 04:08:17 +0200

    Registered a memory-freeing function with atexit() on Windows to ensure 'fullPath' is freed, and for Mac have implemented fork() to free the final memory. Have yet to test the program on Mac.

--- MovieWishlistLauncher.c
+++ MovieWishlistLauncher.c
@@ -7,6 +7,10 @@
 #include <unistd.h>
 
 #ifdef _WIN32
+wchar_t *fullPathFinalPointer;
+void final_free() {
+    free(fullPathFinalPointer);
+}
 #include <shlobj.h>
 #else
 #include <sys/stat.h>
@@ -22,7 +26,7 @@ typedef enum {
 } bool;
 
 #ifdef _WIN32
-size_t wcslen_c(const wchar_t *str) {
+size_t wcslen_c(const wchar_t *restrict str) {
     if (str == NULL || *str == 0) {
         return 0;
     }
@@ -31,7 +35,7 @@ size_t wcslen_c(const wchar_t *str) {
     return count;
 }
 
-size_t wcscat_c(wchar_t *strDest, const wchar_t *strSource) {
+size_t wcscat_c(wchar_t *restrict strDest, const wchar_t *restrict strSource) {
     if (strDest == NULL || strSource == NULL || *strSource == 0) {
         return 0;
     }
@@ -45,7 +49,7 @@ size_t wcscat_c(wchar_t *strDest, const wchar_t *strSource) {
     return count;
 }
 #else
-size_t strlen_c(const char *str) {
+size_t strlen_c(const char *restrict str) {
     if (str == NULL || *str == 0) {
         return 0;
     }
@@ -54,7 +58,7 @@ size_t strlen_c(const char *str) {
     return count;
 }
 
-size_t strcat_c(char *strDest, const char *strSource) {
+size_t strcat_c(char *restrict strDest, const char *restrict strSource) {
     if (strDest == NULL || strSource == NULL || *strSource == 0) {
         return 0;
     }
@@ -68,7 +72,7 @@ size_t strcat_c(char *strDest, const char *strSource) {
     return count;
 }
 #endif
-void memset_c(void *str, size_t num, char c) {
+void memset_c(void *restrict str, size_t num, char c) {
     char *ptr = (char *) str;
     while (num --> 0) {
         *ptr++ = c;
@@ -111,6 +115,7 @@ bool move_file(const char *src, const char *dst) {
 
 int main() {
 #ifdef _WIN32
+    atexit(final_free); // only way to free malloc'ed fullPath pointer on Windows (can't fork())
     wchar_t *homePath;
     homePath = malloc(MAX_PATH*sizeof(wchar_t));
     if (SHGetFolderPathW(NULL, CSIDL_PROFILE, NULL, SHGFP_TYPE_CURRENT, homePath) != S_OK) {
@@ -130,29 +135,30 @@ int main() {
 #ifdef _WIN32
     wchar_t *fullPath;
     fullPath = malloc((wcslen_c(homePath) + 19)*sizeof(wchar_t));
+    fullPathFinalPointer = fullPath;
     *fullPath = 0;
     wcscat_c(fullPath, homePath);
     free(homePath);
     wcscat_c(fullPath, L"\\MovieWishList.jar");
     unsigned calc = (14 + wcslen_c(fullPath))*sizeof(wchar_t);
-    wchar_t *fullCommand = malloc(calc);
-    memset_c(fullCommand, calc, 0);
-    wcscat_c(fullCommand, L"javaw -jar \"");
-    wcscat_c(fullCommand, fullPath);
-    wcscat_c(fullCommand, L"\"");
+    // wchar_t *fullCommand = malloc(calc);
+    // memset_c(fullCommand, calc, 0);
+    // wcscat_c(fullCommand, L"javaw -jar \"");
+    // wcscat_c(fullCommand, fullPath);
+    // wcscat_c(fullCommand, L"\"");
 #else
     char *fullPath;
     fullPath = malloc((strlen_c(homePath) + 19)*sizeof(char));
     *fullPath = 0;
     wcscat_c(fullPath, homePath);
     free(homePath);
-    strcat_c(fullPath, L"\\MovieWishList.jar");
+    strcat_c(fullPath, "\\MovieWishList.jar");
     unsigned calc = (14 + strlen_c(fullPath))*sizeof(char);
-    char *fullCommand = malloc(calc);
-    memset_c(fullCommand, calc, 0);
-    strcat_c(fullCommand, L"javaw -jar \"");
-    strcat_c(fullCommand, fullPath);
-    strcat_c(fullCommand, L"\"");
+    // char *fullCommand = malloc(calc);
+    // memset_c(fullCommand, calc, 0);
+    // strcat_c(fullCommand, "javaw -jar \"");
+    // strcat_c(fullCommand, fullPath);
+    // strcat_c(fullCommand, "\"");
 #endif
 #ifdef _WIN32
     DWORD fileAttr = GetFileAttributesW(fullPath);
@@ -175,6 +181,33 @@ int main() {
     }
 #else
     struct stat64 buff = {};
+    if (stat64(fullPath, &buff) == -1) {
+        if (!move_file("MovieWishList.jar", fullPath)) {
+            fprintf(stderr, ".jar file could not be moved to home directory.")
+            return -1;
+        }
+    }
+    else if (S_ISDIR(buff.st_mode)) {
+        fprintf(stderr, "Target .jar file is a directory.")
+        return -1;
+    }
+    intptr_t retval;
+    const wchar_t * const argv[] = {"javaw", "-jar", fullPath, NULL};
+    pid_t pid = fork();
+    if (pid < 0) {
+        free(fullPath);
+        fprintf(stderr, "fork() error.\n");
+        return -1;
+    if (pid == 0) {
+        if ((retval = _wexecvp("javaw", argv)) == -1) {
+            fprintf(stderr, "Please ensure you have the latest version of the JRE (Java Runtime Environment).")
+            return (int) retval;
+        }
+    }
+    else {
+        pid = wait(NULL);
+        free(fullPath);
+    }
 #endif
     return 0;
 }
